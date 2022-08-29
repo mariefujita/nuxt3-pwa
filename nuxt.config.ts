5104c606559a756157676a8f8861b1422c6fa3c1
Marie Fujita <mariefujita94@gmail.com>
2022-08-29 12:48:56 +0900

    fix config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,17 +2,12 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  buildModules: ['@nuxtjs/pwa'],
+  modules: ['@nuxtjs/pwa'],
   pwa: {
-    meta: {
-      mobileApp: true,
-      mobileAppIOS: true,
-      appleStatusBarStyle: 'black',
-    },
     manifest: {
       lang: 'ja',
-      name: 'Nuxt3 pwa',
-      shortName: 'nuxt pwa',
+      name: 'Nuxt3 with PWA',
+      shortName: 'nuxt3 pwa',
       background_color: '#fff'
     }
   }
