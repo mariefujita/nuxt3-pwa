a66466bee33f523072b19c7cfd1d33e4c2380084
Marie Fujita <mariefujita94@gmail.com>
2022-08-29 13:48:28 +0900

    fix pwa config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,11 +4,22 @@ import { defineNuxtConfig } from 'nuxt'
 export default defineNuxtConfig({
   buildModules: ['@nuxtjs/pwa'],
   pwa: {
+    icon: {
+      source: '/icon.png',    // このファイルを元に各サイズ毎のアイコン画像を自動生成してくれる
+      fileName: 'icon.png',
+      sizes: [64, 120, 144, 152, 192, 384, 512],
+      targetDir: 'icons',
+      plugin: true,
+    },
     manifest: {
       lang: 'ja',
-      name: 'Nuxt3 with PWA',
-      shortName: 'nuxt3 pwa',
-      background_color: '#fff'
+      name: "Nuxt with PWA",
+      short_name: "PWA/Nuxt",
+      display: 'standalone',
+      theme_color: '#F11010'
+    },
+    workbox: {
+      dev: true // or use a global variable to track the current NODE_ENV, etc to determine dev mode
     }
   }
 })
