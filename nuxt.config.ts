002692258e0ba9b1555e4e66d6f257ab3b655fc0
Marie Fujita <mariefujita94@gmail.com>
2022-08-29 14:53:39 +0900

    fix pwa config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,19 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@kevinmarrec/nuxt-pwa'],
+  ssr: true,
+  buildModules: ['@nuxtjs/pwa'],
   pwa: {
+    icon: {
+      source: '/icon.png',
+      fileName: 'icon.png',
+      sizes: [64, 120, 144, 152, 192, 384, 512]
+    },
+    meta: {
+      mobileAppIOS: true,
+      author: 'Me',
+      lang: 'ja'
+    },
     manifest: {
       lang: 'ja',
       name: "Nuxt with PWA",
