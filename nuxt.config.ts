8e8b0d87afdf4c6b027db2d71389fe3eb25b08b6
Marie Fujita <mariefujita94@gmail.com>
2022-08-29 15:23:12 +0900

    set start url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,16 +7,21 @@ export default defineNuxtConfig({
     '@kevinmarrec/nuxt-pwa'
   ],
   pwa: {
+    icon: {
+      source: '/icon.png'
+    },
     meta: {
       mobileAppIOS: true,
       lang: 'ja'
     },
     manifest: {
       lang: 'ja',
-      name: "Nuxt with PWA",
-      short_name: "PWA/Nuxt",
+      name: 'Nuxt with PWA',
+      short_name: 'PWA/Nuxt',
       display: 'standalone',
-      theme_color: '#F11010'
+      theme_color: '#F11010',
+      start_url: 'https://nuxt3-pwa-seven.vercel.app/',
+      background_color: '#fff'
     },
     workbox: {
       enabled: true
