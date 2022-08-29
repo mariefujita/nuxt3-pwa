b1b45e230d04231abb6603771fcc7bef53d7c1c9
Marie Fujita <mariefujita94@gmail.com>
2022-08-29 14:28:06 +0900

    install nuxt3-pwa module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,15 +2,8 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  buildModules: ['@nuxtjs/pwa'],
+  modules: ['@kevinmarrec/nuxt-pwa'],
   pwa: {
-    icon: {
-      source: '/icon.png',    // このファイルを元に各サイズ毎のアイコン画像を自動生成してくれる
-      fileName: 'icon.png',
-      sizes: [64, 120, 144, 152, 192, 384, 512],
-      targetDir: 'icons',
-      plugin: true,
-    },
     manifest: {
       lang: 'ja',
       name: "Nuxt with PWA",
@@ -19,7 +12,7 @@ export default defineNuxtConfig({
       theme_color: '#F11010'
     },
     workbox: {
-      dev: true // or use a global variable to track the current NODE_ENV, etc to determine dev mode
+      enabled: true
     }
   }
 })
