71aea5db477861107071e72753cb894c5a7615ce
Marie Fujita <mariefujita94@gmail.com>
2022-11-02 11:14:54 +0900

    update pwa icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,33 +1,31 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from 'nuxt';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  ssr: true,
-  modules: [
-    '@kevinmarrec/nuxt-pwa'
-  ],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
-  pwa: {
-    meta: {
-      mobileAppIOS: true,
-      lang: 'ja'
-    },
-    manifest: {
-      lang: 'ja',
-      name: 'Nuxt3 SSR with PWA',
-      short_name: 'PWA/Nuxt3',
-      display: 'standalone',
-      background_color: '#111827',
-      theme_color: '#3B82F6',
-      start_url: 'https://nuxt3-pwa-seven.vercel.app/'
-    },
-    workbox: {
-      enabled: false
-    }
-  }
-})
+	ssr: true,
+	modules: ['@kevinmarrec/nuxt-pwa'],
+	postcss: {
+		plugins: {
+			tailwindcss: {},
+			autoprefixer: {},
+		},
+	},
+	pwa: {
+		meta: {
+			mobileAppIOS: true,
+			lang: 'ja',
+		},
+		manifest: {
+			lang: 'ja',
+			name: 'Nuxt3 SSR with PWA',
+			short_name: 'PWA/Nuxt3',
+			display: 'standalone',
+			background_color: '#111827',
+			theme_color: '#3B82F6',
+			start_url: 'https://nuxt3-pwa-seven.vercel.app/',
+		},
+		workbox: {
+			enabled: true,
+		},
+	},
+});
